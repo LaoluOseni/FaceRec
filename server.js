db647ce6fa08d6d2cbdb868c69e51a5046904bad
Laolu Oseni <63481526+LaoluOseni@users.noreply.github.com>
2021-03-04 23:49:22 +0100

    final facerec app

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bcrypt = require('bcryptjs');
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'laoluoseni',
+      password : '',
+      database : 'facerec'
+    }
+  });
+
+//knex is used to link server side logic with database.
+
+//controllers for requests
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
+
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//path handlers
+app.get('/', (req, res) => { res.send(knex.users) });
+
+app.post('/signin', signin.handleSignin(knex, bcrypt));
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, knex) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, knex) })
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3000, () => {
+    console.log('app is running')
+});
