937401ec74efff25c2d945358e6a4ceaee41c25f
Laolu Oseni <63481526+LaoluOseni@users.noreply.github.com>
2021-03-15 21:17:58 +0100

    header

--- controllers/image.js
+++ controllers/image.js
@@ -9,6 +9,7 @@ const app = new Clarifai.App({
 const handleApiCall =(req, res) => {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.json(data);
     })
     //.catch(err => res.json(err))
