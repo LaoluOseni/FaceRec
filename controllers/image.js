7f501f4f6be2ba21e2e63be2979c4de087447d6f
Laolu Oseni <63481526+LaoluOseni@users.noreply.github.com>
2021-03-15 18:49:01 +0100

    api woes

--- controllers/image.js
+++ controllers/image.js
@@ -7,11 +7,12 @@ const app = new Clarifai.App({
 });
 
 const handleApiCall =(req, res) => {
-    app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
+        console.log(data);
         res.json(data);
     })
-    .catch(err => res.json(err))
+    //.catch(err => res.json(err))
     //.catch(err => res.status(400).json('unable to work with API'))
 }
 const handleImage = (req, res, knex) => {
