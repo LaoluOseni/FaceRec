ac417ab197ce18a9dfae12976846a71ea43e8a02
Laolu Oseni <63481526+LaoluOseni@users.noreply.github.com>
2021-03-15 17:59:53 +0100

    new

--- controllers/image.js
+++ controllers/image.js
@@ -4,10 +4,10 @@ const Clarifai = require('clarifai');
 const app = new Clarifai.App({
     apiKey: '517997cb8ff044d8bcdfcb32c22c9422'
     //api key and image detection is implemented at backend for security
-})
+});
 
 const handleApiCall =(req, res) => {
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
     .then(data => {
         res.json(data);
     })
